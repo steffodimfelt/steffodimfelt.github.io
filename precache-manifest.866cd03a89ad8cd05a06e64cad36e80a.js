425889c3a5156d1c295e63b1c71c78c80e135370
Steffo <steffo.dimfelt@gmail.com>
2018-12-06 23:17:28 +0100

    Initial test

---
+++ precache-manifest.866cd03a89ad8cd05a06e64cad36e80a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "763d527e2d295ada579b",
+    "url": "/static/js/main.763d527e.chunk.js"
+  },
+  {
+    "revision": "82dafdb578ba27673946",
+    "url": "/static/js/1.82dafdb5.chunk.js"
+  },
+  {
+    "revision": "763d527e2d295ada579b",
+    "url": "/static/css/main.90ba962f.chunk.css"
+  },
+  {
+    "revision": "77b2935f6a009acd2ff16f27bfa74ec5",
+    "url": "/index.html"
+  }
+];
